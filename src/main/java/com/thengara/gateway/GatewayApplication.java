b5102c0a1adfebd5459ad2de6ecef55b3f0d7fc7
Shiju John <shijujohn1093@gmail.com>
2018-05-01 23:05:39 +0530

    first commit

---
+++ src/main/java/com/thengara/gateway/GatewayApplication.java
@@ -0,0 +1,36 @@
+package com.thengara.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.thengara.gateway.filter.ErrorFilter;
+import com.thengara.gateway.filter.PostFilter;
+import com.thengara.gateway.filter.PreFilter;
+import com.thengara.gateway.filter.RouteFilter;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+	@Bean
+    public PreFilter preFilter() {
+        return new PreFilter();
+    }
+    @Bean
+    public PostFilter postFilter() {
+        return new PostFilter();
+    }
+    @Bean
+    public ErrorFilter errorFilter() {
+        return new ErrorFilter();
+    }
+    @Bean
+    public RouteFilter routeFilter() {
+        return new RouteFilter();
+    }
+}
